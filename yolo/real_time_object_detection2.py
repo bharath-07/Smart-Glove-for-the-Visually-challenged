b65cf0a75f0827edd2d64625645eb9cf9b7cae7e
Bharath Surianarayanan <31988151+bharath-07@users.noreply.github.com>
2020-03-09 22:35:15 +0530

    Add files via upload

---
+++ yolo/real_time_object_detection2.py
@@ -0,0 +1,206 @@
+# USAGE
+# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import re
+import speech_recognition as sr 
+from gtts import gTTS   
+import os 
+ty=0
+mytext = 'Please Speak now'
+  
+# Language in which you want to convert 
+language = 'en'
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+file="objdet.mp3"
+myobj.save("objdet.mp3")
+
+mic_name = "USB2.0 PC CAMERA: Audio (hw:1,0)"
+
+sample_rate = 48000
+chunk_size = 2048
+r = sr.Recognizer() 
+  
+mic_list = sr.Microphone.list_microphone_names() 
+  
+for i, microphone_name in enumerate(mic_list): 
+    if microphone_name == mic_name: 
+        device_id = i 
+  
+with sr.Microphone(device_index = device_id, sample_rate = sample_rate,  
+                        chunk_size = chunk_size) as source: 
+    r.adjust_for_ambient_noise(source) 
+    # Playing the converted file
+    
+    
+    os.system("mpg123 " + file)
+    print ("Say Something")
+    audio = r.listen(source) 
+          
+    try: 
+        text = r.recognize_google(audio) 
+        print ("you said: " + text) 
+      
+      
+    except sr.UnknownValueError: 
+        print("Google Speech Recognition could not understand audio") 
+      
+
+
+
+
+
+
+
+
+
+
+ct=0
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"glass", "bus", "car", "cat", "seat", "cow", "table",
+	"dog", "horse", "motorbike", "mum", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+
+
+
+
+
+
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# extract the index of the class label from the
+			# `detections`, then compute the (x, y)-coordinates of
+			# the bounding box for the object
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			#print(label)
+			x=label.split(":")
+				
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+			for i in x:
+				k=''.join(i)
+				if(k==text):
+					lk=(startX+endX)/2
+					rk=(startY+endY)/2
+					ct=1
+					if(lk>300):
+						ty=1
+					elif (lk>180 and lk <=300):
+						ty=2
+					elif (lk<=180):
+						ty=3
+							
+        	
+  
+# Language in which you want to convert 
+        
+	if (ty==1):
+		mytext='Object was found and is to the right'
+	elif (ty==2):
+		mytext='Object was found and is in the center'
+	elif (ty==3):
+		mytext='Object was found and is to the left'					
+				
+	language = 'en'
+	myobj = gTTS(text=mytext, lang=language, slow=False)
+	file="objdet.mp3"
+	myobj.save("objdet.mp3")
+				
+					
+            
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		print("You quit")
+		break
+	elif ct==1:
+		os.system("mpg123 " + file)
+		print("Object was found")
+		
+		#print(lk)
+		#print(rk)
+		
+		
+		break	
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
